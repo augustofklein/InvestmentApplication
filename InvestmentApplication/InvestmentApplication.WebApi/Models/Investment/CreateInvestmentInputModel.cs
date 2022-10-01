1e9ca8b283da84439fff3300f071c4c1a22ac9b0
Augusto.Klein <augusto_klein@hotmail.com>
2022-10-01 12:02:33 -0300

    Adicao das classes de inputmodel e command da criacao do investimento

---
+++ InvestmentApplication/InvestmentApplication.WebApi/Models/Investment/CreateInvestmentInputModel.cs
@@ -0,0 +1,31 @@
+﻿using CSharpFunctionalExtensions;
+using System;
+
+namespace InvestmentApplication.WebApi.Models.Investment
+{
+    public class CreateInvestmentInputModel
+    {
+        public string AssetName { get; private set; }
+        public DateTime OperationDate { get; private set; }
+        public char OperationType { get; private set; }
+        public decimal OperationAmount { get; private set; }
+        public decimal AssetUnitPrice { get; private set; }
+        public decimal TransactionTaxes { get; private set; }
+
+        public CreateInvestmentInputModel(string assetName, DateTime operationDate, char operationType, decimal operationAmount, decimal assetUnitPrice, decimal transactionTaxes)
+        {
+            AssetName = assetName;
+            OperationDate = operationDate;
+            OperationType = operationType;
+            OperationAmount = operationAmount;
+            AssetUnitPrice = assetUnitPrice;
+            TransactionTaxes = transactionTaxes;
+        }
+
+        public Result<CreateInvestmentCommand> CreateCommand()
+        {
+            var investmentApplicationCommand = new CreateInvestmentCommand(AssetName, OperationDate, OperationType, OperationAmount, AssetUnitPrice, TransactionTaxes);
+            return investmentApplicationCommand;
+        }
+    }
+}
---
+++ InvestmentApplication/InvestmentApplication/Domain/Investment/Commands/CreateInvestmentCommand.cs
@@ -0,0 +1,30 @@
+﻿using CSharpFunctionalExtensions;
+using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InvestmentApplication.Domain.Investment.Commands
+{
+    public sealed class CreateInvestmentCommand : IRequest<Result>
+    {
+        public string AssetName { get; private set; }
+        public DateTime OperationDate { get; private set; }
+        public char OperationType { get; private set; }
+        public decimal OperationAmount { get; private set; }
+        public decimal AssetUnitPrice { get; private set; }
+        public decimal TransactionTaxes { get; private set; }
+
+        public CreateInvestmentCommand(string assetName, DateTime operationDate, char operationType, decimal operationAmount, decimal assetUnitPrice, decimal transactionTaxes)
+        {
+            AssetName = assetName;
+            OperationDate = operationDate;
+            OperationType = operationType;
+            OperationAmount = operationAmount;
+            AssetUnitPrice = assetUnitPrice;
+            TransactionTaxes = transactionTaxes;
+        }
+    }
+}
